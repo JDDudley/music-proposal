04bace9c9043e57e624163f7f49f8fa7a0d57f78
jddudley <jason@jddudley.com>
2019-05-08 20:48:46 -0600

    Cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="../assets/logo.png">
-  </q-page>
+  <div class="row">
+    <h4>This is where we put proposal</h4>
+    <p>Could integrate links into the notes above or something cool and interactive</p>
+    <p>Demonstrate the ability to make an interactive design that works with whatever is needed</p>
+  </div>
 </template>
 
 <style>
