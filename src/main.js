d998841955d4b80de18e29f10e8c9fa9e466e29c
Jason Dudley <jason@jddudley.com>
2019-05-10 09:34:42 -0600

    Add Vue Router

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './styles/quasar.styl'
 import 'quasar-extras/material-icons'
 import 'quasar-extras/fontawesome'
 import Quasar from 'quasar'
+import router from './router'
 
 Vue.use(Quasar, {
   config: {}
@@ -13,5 +14,6 @@ Vue.use(Quasar, {
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
